20cdb3f8b313b06ae673689be8ed003136a1f9e1
kavyaNarkar <narkarp.kavya.com>
2026-01-08 22:37:17 +0530

    refactor: move issue form to Report page and fix blank screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-// src/App.jsx
 import React from "react";
-import ReportFormPage from "./issueForm/form.jsx";
+import Report from "./pages/Report";
 
 const App = () => {
-  return (
-    <div className="min-h-screen">
-      <div className="p-4">
-        <div className="container">
-          <ReportFormPage />
-        </div>
-      </div>
-    </div>
-  );
+  return <Report />;
 };
 
 export default App;
---
+++ src/pages/Report.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReportFormPage from "../issueForm/form";
+
+const Report = () => {
+  return (
+    <div className="min-h-screen p-4">
+      <div className="container mx-auto">
+        <ReportFormPage />
+      </div>
+    </div>
+  );
+};
+
+export default Report;
