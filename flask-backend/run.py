e59673cb40afcddb9dc5c69e8a86d49dd0069637
kavyaNarkar <narkarp.kavya.com>
2026-03-22 15:00:33 +0530

    update flask host for production

--- flask-backend/run.py
+++ flask-backend/run.py
@@ -4,5 +4,5 @@ import os
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.getenv("PORT", 5005))
-    app.run(host='127.0.0.1', port=port, debug=False)
+    port = int(os.getenv("PORT", 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
